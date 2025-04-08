fa25df02a7175458f95f2c6275bbe8281b9d1102
ccoun <ccoun@tulane.edu>
2025-04-07 21:08:28 -0500

    Update main.py

--- main.py
+++ main.py
@@ -19,11 +19,52 @@ def MED(S, T):
             return(1 + min(MED(S, T[1:]), MED(S[1:], T)))
 
 
-def fast_MED(S, T, MED={}):
+def fast_MED(S, T, memo=None):
     # TODO -  implement top-down memoization
-    pass
+    if memo is None:
+        memo = {}
+    if (S, T) in memo:
+        return memo[(S, T)]
 
-def fast_align_MED(S, T, MED={}):
-    # TODO - keep track of alignment
-    pass
+    if not S:
+        return len(T)
+    if not T:
+        return len(S)
+
+    if S[0] == T[0]:
+        memo[(S, T)] = fast_MED(S[1:], T[1:], memo)
+    else:
+        insert = fast_MED(S, T[1:], memo) + 1
+        delete = fast_MED(S[1:], T, memo) + 1
+        memo[(S, T)] = min(insert, delete)
+
+    return memo[(S, T)]
+
+
+def fast_align_MED(S, T, memo=None):
+    if memo is None:
+        memo = {}
+
+    if (S, T) in memo:
+        return memo[(S, T)][1:]  # return only alignments
+
+    if S == "":
+        result = (len(T), "-" * len(T), T)
+    elif T == "":
+        result = (len(S), S, "-" * len(S))
+    elif S[0] == T[0]:
+        dist, align_S, align_T = fast_align_MED(S[1:], T[1:], memo)
+        result = (dist, S[0] + align_S, T[0] + align_T)
+    else:
+        sub_dist, sub_S, sub_T = fast_align_MED(S[1:], T[1:], memo)
+        sub = (1 + sub_dist, S[0] + sub_S, T[0] + sub_T)
+        ins_dist, ins_S, ins_T = fast_align_MED(S, T[1:], memo)
+        ins = (1 + ins_dist, "-" + ins_S, T[0] + ins_T)
+        del_dist, del_S, del_T = fast_align_MED(S[1:], T, memo)
+        delete = (1 + del_dist, S[0] + del_S, "-" + del_T)
+
+        result = min([sub, ins, delete], key=lambda x: x[0])
+
+    memo[(S, T)] = result
+    return result[1:]
 
